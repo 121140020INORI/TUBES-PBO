a29a3842db1a82dbd0459de919267cfdf061824d
Kaben011201 <126554385+Kaben011201@users.noreply.github.com>
2023-05-12 16:45:22 +0700

    Update lagi.py

--- lagi.py
+++ lagi.py
@@ -139,18 +139,73 @@ class Reflection:
 		elif ball.coorX - ball.diameter >= WIDTH:
 			return 'player1'
 
+class Button:
+    def __init__(self, txt, coorx, coory):
+        self.text = txt
+        self.coorx = coorx
+        self.coory = coory
+        self.button = pygame.rect.Rect((self.coorx, self.coory), (253, 40))
+        
+    def draw(self):
+        pygame.draw.rect(window, WHITE, self.button, 0, 5)
+        #                                  coor x        coor y     w    h   border
+        pygame.draw.rect(window, BLACK, [self.coorx, self.coory, 253, 40], 5, 5)
+        text2 = font.render(self.text, True, BLACK)
+        window.blit(text2, (self.coorx + 65, self.coory + 7))
+        
+    def check_clicked(self):
+        if self.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+            return True
+        else:
+            return False
 
 WIDTH, HEIGHT = 900, 500
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+PURPLE = (128, 0, 128)
 
 pygame.init()
 window = pygame.display.set_mode( (WIDTH, HEIGHT) )
 pygame.display.set_caption('PONG')
+main_menu=False
+font = pygame.font.SysFont("monospace", 24, bold=True)
+menu_command=0
+
+#gambar pada menu
+image1 = pygame.image.load("opening.png")
+image2 = pygame.image.load("info.png")
+
+def draw_game():
+    menu_btn = Button('Main Menu', WIDTH//3 + 18, HEIGHT-140)
+    menu_btn.draw()
+    menu = menu_btn.check_clicked()
+    return menu
+
+def draw_menu():
+    window.fill( PURPLE )
+    command = -1
+    pygame.draw.rect(window, PURPLE, [100, 100, 300, 300])
+    pygame.draw.rect(window, BLACK, [100, 100, 290, 280], 5)
+    tema = pygame.font.SysFont("monospace", 40, bold=True)
+    txt = tema.render("PONG GAME", True, WHITE)
+    window.blit(txt, (142, 127))
+    # menu exit button
+    menu = Button("Exit Menu", 120, 300)
+    menu.draw()
+    button1 = Button("Play Game", 120, 180)
+    button1.draw()
+    button2 = Button("Information", 120, 240)
+    button2.draw()
+    if menu.check_clicked():
+        command = 0
+    if button1.check_clicked():
+        command = 1
+    if button2.check_clicked():
+        command = 2
+    return command
 
 def draw_board():
-	window.fill( BLACK )
+	window.fill( PURPLE )
 	pygame.draw.line(window, WHITE, (WIDTH//2, 0), (WIDTH//2, HEIGHT), 5)
   
 def restart():
@@ -160,12 +215,10 @@ def restart():
 	ball.restart_coor()
 	racket1.restart_coor()
 	racket2.restart_coor()
- 
-draw_board()
 
 def show_game_over():
     font = pygame.font.SysFont(None, 48)
-    text = font.render("Game Over", True, (255, 255, 255))
+    text = font.render("Game Over", True, WHITE)
     text_rect = text.get_rect(center=(WIDTH//2, HEIGHT//2))
     window.blit(text, text_rect)
     pygame.display.update()
@@ -181,94 +234,110 @@ playing = False
 clock = pygame.time.Clock()
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_p and not playing:
-				ball.start()
-				playing = True
+		#tampilkan menu
+        window.fill(PURPLE)
+        window.blit(image1, (WIDTH//3 + 20, HEIGHT//2 - 100))
 
-			if event.key == pygame.K_r and playing:
-				restart()
-				playing = False
+        if main_menu:
+            menu_command = draw_menu()
+            if menu_command != -1:
+                main_menu = False
+                
+        else:
+            main_menu = draw_game()
+            if menu_command == 1:
+                pygame.draw.rect(window, PURPLE, [100, 100, WIDTH, 300])
+                text = font.render('Lets play the game', True, 'black')
+                ball.start()
+                playing = True
+                window.blit(text, (150, 100))
+            if menu_command == 2:
+                window.blit(image2, (WIDTH//3 + 20, HEIGHT//2 - 150))
 
-			if event.key == pygame.K_w:
-				racket1.state = 'up'
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r and playing:
+                restart()
+                playing = False
 
-			if event.key == pygame.K_s:
-				racket1.state = 'down'
+            if event.key == pygame.K_w:
+                racket1.state = 'up'
 
-			if event.key == pygame.K_UP:
-				racket2.state = 'up'
+            if event.key == pygame.K_s:
+                racket1.state = 'down'
 
-			if event.key == pygame.K_DOWN:
-				racket2.state = 'down'
+            if event.key == pygame.K_UP:
+                racket2.state = 'up'
 
-		if event.type == pygame.KEYUP:
-			racket1.state = 'stopped'
-			racket2.state = 'stopped'
+            if event.key == pygame.K_DOWN:
+                racket2.state = 'down'
 
-	if playing == True:
-		draw_board()
+        if event.type == pygame.KEYUP:
+            racket1.state = 'stopped'
+            racket2.state = 'stopped'
 
-		# ball
-		ball.move()
-		ball.draw()
+    if playing:
+        draw_board()
 
-		# racket 1
-		racket1.move()
-		racket1.clamp()
-		racket1.draw()
+        # ball
+        ball.move()
+        ball.draw()
 
-		# racket 2
-		racket2.move()
-		racket2.clamp()
-		racket2.draw()
+        # racket 1
+        racket1.move()
+        racket1.clamp()
+        racket1.draw()
 
-		# wall reflection
-		if reflection.between_ball_and_walls(ball):
-			ball.wall_collision()
+        # racket 2
+        racket2.move()
+        racket2.clamp()
+        racket2.draw()
 
-		# racket reflection
-		if racket1 or racket2:
-			if reflection.between_ball_and_racket(ball, racket1):
-				ball.racket_collision()
-			if reflection.between_ball_and_racket(ball, racket2):
-				ball.racket_collision()
+        # wall reflection
+        if reflection.between_ball_and_walls(ball):
+            ball.wall_collision()
 
-		# GOAL OF PLAYER !
-		goal_player = reflection.check_goal(ball)
-		if goal_player:
-			draw_board()
-			if goal_player == 'player1':
-				cek=score1.increase()
-				if cek == "Winner":
-					score1.show()
-					pygame.time.wait(1000)
-					window.fill( BLACK )
-					show_game_over()
-					pygame.time.wait(1000)
-					sys.exit()
-					
-			else:
-				cek=score2.increase()
-				if cek == "Winner":
-					score2.show()
-					pygame.time.wait(1000)
-					window.fill( BLACK )
-					show_game_over()
-					pygame.time.wait(1000)
-					sys.exit()
-     
-			ball.restart_coor()
-			racket1.restart_coor()
-			racket2.restart_coor()
-			playing = False
+        # racket reflection
+        if racket1 or racket2:
+            if reflection.between_ball_and_racket(ball, racket1):
+                ball.racket_collision()
+            if reflection.between_ball_and_racket(ball, racket2):
+                ball.racket_collision()
 
-	score1.show()
-	score2.show()
+        # GOAL OF PLAYER !
+        goal_player = reflection.check_goal(ball)
+        if goal_player:
+            draw_board()
+            if goal_player == 'player1':
+                cek = score1.increase()
+                if cek == "Winner":
+                    score1.show()
+                    pygame.time.wait(1000)
+                    window.fill(PURPLE)
+                    show_game_over()
+                    pygame.time.wait(1000)
+                    sys.exit()
 
-	clock.tick(40)
-	pygame.display.update()
+            else:
+                cek = score2.increase()
+                if cek == "Winner":
+                    score2.show()
+                    pygame.time.wait(1000)
+                    window.fill(PURPLE)
+                    show_game_over()
+                    pygame.time.wait(1000)
+                    sys.exit()
+
+            ball.restart_coor()
+            racket1.restart_coor()
+            racket2.restart_coor()
+            playing = False
+
+            score1.show()
+            score2.show()
+
+    clock.tick(40)
+    pygame.display.update()
